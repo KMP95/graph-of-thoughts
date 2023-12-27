5b0b97d2848d5846941c6cdb786d32f42c3333d3
Kevin Monsálvez <kevin.monsalvez@itcl.es>
2023-12-27 10:10:35 +0100

    Modification of the chatgpt class to manage the Token per Day and Minute limits.

--- graph_of_thoughts/language_models/chatgpt.py
+++ graph_of_thoughts/language_models/chatgpt.py
@@ -77,7 +77,7 @@ class ChatGPT(AbstractLanguageModel):
             return self.respone_cache[query]
 
         if num_responses == 1:
-            response = self.chat([{"role": "user", "content": query}], num_responses)
+            response = self.chat([{"role": "system", "content": "You are a helpful assistant, expert in writing States of the Art (SOTA)"}, {"role": "user", "content": query}], num_responses)
         else:
             response = []
             next_try = num_responses
@@ -85,14 +85,14 @@ class ChatGPT(AbstractLanguageModel):
             while num_responses > 0 and total_num_attempts > 0:
                 try:
                     assert next_try > 0
-                    res = self.chat([{"role": "user", "content": query}], next_try)
+                    res = self.chat([{"role": "system", "content": "You are a helpful assistant, expert in writing States of the Art (SOTA)"}, {"role": "user", "content": query}], next_try)
                     response.append(res)
                     num_responses -= next_try
                     next_try = min(num_responses, next_try)
                 except Exception as e:
                     next_try = (next_try + 1) // 2
                     self.logger.warning(
-                        f"Error in chatgpt: {e}, trying again with {next_try} samples"
+                        f"Error in chatgpt: {e}, trying again with {next_try} samples. If the error is about Tokens per Day limit, do not worry, instead of using GPT4, the lower version GPT3.5-Turbo will be used instead."
                     )
                     time.sleep(random.randint(1, 3))
                     total_num_attempts -= 1
@@ -114,28 +114,91 @@ class ChatGPT(AbstractLanguageModel):
         :return: The OpenAI model's response.
         :rtype: ChatCompletion
         """
-        response = self.client.chat.completions.create(
-            model=self.model_id,
-            messages=messages,
-            temperature=self.temperature,
-            max_tokens=self.max_tokens,
-            n=num_responses,
-            stop=self.stop,
-        )
+        try:            
+            response = self.client.chat.completions.create(
+                model=self.model_id,
+                messages=messages,
+                temperature=self.temperature,
+                max_tokens=self.max_tokens,
+                n=num_responses,
+                stop=self.stop,
+            )
 
-        self.prompt_tokens += response.usage.prompt_tokens
-        self.completion_tokens += response.usage.completion_tokens
-        prompt_tokens_k = float(self.prompt_tokens) / 1000.0
-        completion_tokens_k = float(self.completion_tokens) / 1000.0
-        self.cost = (
-            self.prompt_token_cost * prompt_tokens_k
-            + self.response_token_cost * completion_tokens_k
-        )
-        self.logger.info(
-            f"This is the response from chatgpt: {response}"
-            f"\nThis is the cost of the response: {self.cost}"
-        )
-        return response
+            self.prompt_tokens += response.usage.prompt_tokens
+            self.completion_tokens += response.usage.completion_tokens
+            prompt_tokens_k = float(self.prompt_tokens) / 1000.0
+            completion_tokens_k = float(self.completion_tokens) / 1000.0
+            self.cost = (
+                self.prompt_token_cost * prompt_tokens_k
+                + self.response_token_cost * completion_tokens_k
+            )
+            self.logger.info(
+                f"This is the response from chatgpt: {response}"
+                f"\nThis is the cost of the response: {self.cost}"
+            )
+            return response
+        ### The following code is added to the original class, to change the model from GPT4 to GPT3.5 in case the Tokens per Day (TPD) limit is reached.
+        except OpenAIError as e:
+            if "on tokens_usage_based per day" in e.message:
+                self.logger.warning("Error in token ussage. This error corresponds to TPD")
+                    #    f"Error in chatgpt: {e}, this is the new modified part by Kevin."
+                    #)
+
+                    # Handle rate limit exceeded error here
+                response = self.client.chat.completions.create(
+                    model="gpt-3.5-turbo-16k",
+                    messages=messages,
+                    temperature=1.25,
+                    max_tokens=5000,
+                    n=num_responses,
+                    stop=self.stop,
+                )
+
+                self.prompt_tokens += response.usage.prompt_tokens
+                self.completion_tokens += response.usage.completion_tokens
+                prompt_tokens_k = float(self.prompt_tokens) / 1000.0
+                completion_tokens_k = float(self.completion_tokens) / 1000.0
+                self.cost = (
+                    self.prompt_token_cost * prompt_tokens_k
+                    + self.response_token_cost * completion_tokens_k
+                )
+                self.logger.info(
+                    f"You have exceeded the max. number of tokens per day with GPT4, so the following response has been obtained with GPT3.5. This is the response from GPT3.5: {response}"
+                    f"\nThis is the cost of the response: {self.cost}"
+                )
+                #print("This error corresponds to TPD")
+                return response
+            elif "on tokens_usage_based per min" in e.message:
+                self.logger.warning("Error in token ussage. This error corresponds to TPM")
+                time.sleep(64)
+                response = self.client.chat.completions.create(
+                    model=self.model_id,
+                    messages=messages,
+                    temperature=self.temperature,
+                    max_tokens=self.max_tokens,
+                    n=num_responses,
+                    stop=self.stop,
+                )
+                self.prompt_tokens += response.usage.prompt_tokens
+                self.completion_tokens += response.usage.completion_tokens
+                prompt_tokens_k = float(self.prompt_tokens) / 1000.0
+                completion_tokens_k = float(self.completion_tokens) / 1000.0
+                self.cost = (
+                    self.prompt_token_cost * prompt_tokens_k
+                    + self.response_token_cost * completion_tokens_k
+                )
+                self.logger.info(
+                    f"You have exceeded the max. number of tokens per min with the model you are using. Therefore, a break of a min has been imposed. After that min, the model has been called again. This is the response from the model: {response}"
+                    f"\nThis is the cost of the response: {self.cost}"
+                )   
+                return response
+
+            else:
+                # For other OpenAI errors, raise the exception again
+                self.logger.warning("This is not an error related to Token Limits. Check the error!")
+
+                raise e    
+            
 
     def get_response_texts(
         self, query_response: Union[List[ChatCompletion], ChatCompletion]
